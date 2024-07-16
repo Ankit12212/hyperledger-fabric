9f083de570677338e3d4018a5117b04cc4ca9987
Ankit <regmiank2002@gmail.com>
2024-07-16 16:48:39 +0545

    compiled

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -16,44 +16,58 @@ cryptogen generate --config=${FABRIC_CFG_PATH}/crypto-config-orderer.yaml
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate crypto material for orderer..."
   exit 1
+elif [ "$?" -eq 1 ]; then
+  echo "orderer crypto material generated."
 fi
 
 cryptogen generate --config=${FABRIC_CFG_PATH}/crypto-config-org1.yaml
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate crypto material for org1..."
   exit 1
+elif [ "$?" -eq 1 ]; then
+  echo "Org1 crypto material generated."
 fi
 
 cryptogen generate --config=${FABRIC_CFG_PATH}/crypto-config-org2.yaml
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate crypto material for org2..."
   exit 1
+elif [ "$?" -eq 1 ]; then
+  echo "Org2 crypto material generated."
 fi
 
 # Generate genesis block for orderer
 configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ${FABRIC_CFG_PATH}/genesis.block -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate orderer genesis block..."
-  exit 1
+  #exit 1
+elif [ "$?" -eq 1 ]; then
+  echo "Genesis block for orderer generated"
 fi
 
 # Generate channel configuration transaction
 configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ${FABRIC_CFG_PATH}/channel.tx -channelID $CHANNEL_NAME
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate channel configuration transaction..."
-  exit 1
+  #exit 1
+elif [ "$?" -eq 1 ]; then
+  echo "Channel configuration transaction generated"
 fi
 
 # Generate anchor peer transaction for Org1MSP
 configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ${FABRIC_CFG_PATH}/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Org1MSP..."
-  exit 1
+  #exit 1
+elif [ "$?" -eq 1 ]; then
+  echo "Anchor peer for Org1MSP generated"
 fi
 
 # Generate anchor peer transaction for Org2MSP
 configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ${FABRIC_CFG_PATH}/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
 if [ "$?" -ne 0 ]; then
   echo "Failed to generate anchor peer update for Org2MSP..."
-  exit 1
+  #exit 1
+elif [ "$?" -eq 1 ]; then
+  echo "Anchor peer for Org2MSP generated"
 fi
--- scripts/start-network.sh
+++ scripts/start-network.sh
@@ -12,28 +12,27 @@ CHANNEL_NAME=mychannel
 # Clean the keystore
 rm -rf ./hfc-key-store
 
-# Shut down the Docker containers for the system tests.
+# Shut down any existing Docker containers
 docker-compose -f ../docker-compose.yaml down
 
 # Start the Docker containers for the network
-docker-compose -f ../docker-compose.yaml up -d ca.org1.example.com ca.org2.example.com orderer.example.com peer0.org1.example.com peer0.org2.example.com
+docker-compose -f ../docker-compose.yaml up -d
 
 # Wait for Hyperledger Fabric to start
-# Incase of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
 export FABRIC_START_TIMEOUT=10
 sleep ${FABRIC_START_TIMEOUT}
 
 # Create the channel
-docker exec -e "CORE_PEER_LOCALMSPID=OrdererMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" orderer.example.com peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/mychannel.tx
+# docker exec -e "CORE_PEER_LOCALMSPID=OrdererMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" hyperledger-project_orderer.example.com_1 peer channel create -o hyperledger-project_orderer.example.com_1:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/mychannel.tx
 
-# Join peer0.org1.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" peer0.org1.example.com peer channel join -b $CHANNEL_NAME.block
+# Join peer0.org1.example.com to the channel
+docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" hyperledger-project_peer0.org1.example.com_1 peer channel join -b $CHANNEL_NAME.block
 
-# Join peer0.org2.example.com to the channel.
-docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" peer0.org2.example.com peer channel join -b $CHANNEL_NAME.block
+# Join peer0.org2.example.com to the channel
+docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" hyperledger-project_peer0.org2.example.com_1 peer channel join -b $CHANNEL_NAME.block
 
 # Update anchor peers for Org1MSP
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" peer0.org1.example.com peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/Org1MSPanchors.tx
+# docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" hyperledger-project_peer0.org1.example.com1 peer channel update -o hyperledger-project_orderer.example.com1:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/Org1MSPanchors.tx
 
-# Update anchor peers for Org2MSP
-docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" peer0.org2.example.com peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/Org2MSPanchors.tx
+# # Update anchor peers for Org2MSP
+# docker exec -e "CORE_PEER_LOCALMSPID=Org2MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/peer/" hyperledger-project_peer0.org2.example.com1 peer channel update -o hyperledger-project_orderer.example.com1:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/Org2MSPanchors.tx
